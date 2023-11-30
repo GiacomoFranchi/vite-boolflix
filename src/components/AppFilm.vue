da156efe1f2be04f36bbb3abc35179f364b0ee86
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-11-30 13:39:58 +0100

    add: poster_path in AppFilm

--- src/components/AppFilm.vue
+++ src/components/AppFilm.vue
@@ -17,27 +17,25 @@ export default {
 </script>
 
 <template>
-    <div>
-        
+    <div class="card">
+        <div class="locandina">
+          <img :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" alt="">
+        </div>
+        <div class="info">
         <h2>{{ film.title? film.title : film.name  }}</h2>
         <h3>{{ film.original_title? film.original_title : film.original_name }}</h3>
         <div>
-          <img v-if="flag.includes(film.original_language)"  :src="getImgUrl(film.original_language)" alt="">
+          <img class="flag" v-if="flag.includes(film.original_language)"  :src="getImgUrl(film.original_language)" alt="">
           <p v-else>{{ film.original_language }}</p>
-        </div>
-        
+        </div>        
         <span>{{ film.vote_average }}</span>
+      </div>
     </div>
   </template>
   
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 div{
-  div{
-    width: 25px;
-    img{
-      width: 100%;
-    }
+
   }
-}
 </style>
